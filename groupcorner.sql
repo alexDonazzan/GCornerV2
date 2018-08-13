89c3214b2c5f499fbaea9c283ed32d39eb5e87dc
Alexandre DONAZZAN <alexandre.donazzan@gmail.com>
2018-08-13 18:30:26 +0200

    [base] export de la base avec un petit jeu de donnée

--- groupcorner.sql
+++ groupcorner.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 09 août 2018 à 19:43
+-- Généré le :  lun. 13 août 2018 à 16:28
 -- Version du serveur :  5.7.21
 -- Version de PHP :  7.2.4
 
@@ -31,7 +31,7 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `gcv2contents`;
 CREATE TABLE IF NOT EXISTS `gcv2contents` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `parent_id` int(11) DEFAULT NULL,
+  `parent` int(11) DEFAULT NULL,
   `slug` varchar(150) NOT NULL,
   `type` enum('general','landing_page') DEFAULT NULL,
   `sub_type` enum('event','geo') DEFAULT NULL,
@@ -46,8 +46,17 @@ CREATE TABLE IF NOT EXISTS `gcv2contents` (
   `lastmod` datetime NOT NULL,
   `date_published` datetime NOT NULL,
   `active` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `IDX_C46160443D8E604F` (`parent`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `gcv2contents`
+--
+
+INSERT INTO `gcv2contents` (`id`, `parent`, `slug`, `type`, `sub_type`, `sub_type_id`, `smarty`, `name`, `css_class`, `latitude`, `longitude`, `radius`, `place_str`, `lastmod`, `date_published`, `active`) VALUES
+(1, NULL, 'test', NULL, NULL, NULL, 1, 'test1', 'main.css', 2, 2, 2, 'place des Jacobins', '2018-08-13 00:00:00', '2018-08-14 00:00:00', 1),
+(2, NULL, 'test2', NULL, NULL, NULL, 1, 'test2', 'main.css', 2, 2, 2, 'place des Jacobins', '2018-08-13 00:00:00', '2018-08-14 00:00:00', 1);
 
 -- --------------------------------------------------------
 
@@ -61,24 +70,39 @@ CREATE TABLE IF NOT EXISTS `gcv2contents_i18n` (
   `content_id` int(11) NOT NULL,
   `lang` varchar(5) NOT NULL,
   `slug` varchar(50) NOT NULL,
-  `content` longtext NOT NULL,
   `meta_title` varchar(200) NOT NULL,
   `meta_description` longtext NOT NULL,
   `meta_keywords` varchar(255) NOT NULL,
   `h1_title` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `content_id` (`content_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `FK_Article_Traduction` (`content_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `gcv2contents_i18n`
+--
+
+INSERT INTO `gcv2contents_i18n` (`id`, `content_id`, `lang`, `slug`, `meta_title`, `meta_description`, `meta_keywords`, `h1_title`) VALUES
+(1, 1, 'fr', 'testlang', 'test hotel toto', 'lorem ipsum toto test hotel', 'test toto hotel', 'test'),
+(2, 1, 'fr', 'testlang2', 'test hotel toto titi', 'lorem ipsum toto test hotel titi', 'test toto hotel titi', 'titi'),
+(3, 2, 'fr', 'testlang2', 'hotel toto titi', 'lorem ipsum toto hotel titi', ' toto hotel titi', 'titi'),
+(4, 2, 'fr', 'testlang3', 'hotel toto titi galapagos', 'lorem ipsum toto hotel galapagos', ' toto hotel titi', 'galapagos');
 
 --
 -- Contraintes pour les tables déchargées
 --
 
 --
+-- Contraintes pour la table `gcv2contents`
+--
+ALTER TABLE `gcv2contents`
+  ADD CONSTRAINT `FK_C46160443D8E604F` FOREIGN KEY (`parent`) REFERENCES `gcv2contents` (`id`);
+
+--
 -- Contraintes pour la table `gcv2contents_i18n`
 --
 ALTER TABLE `gcv2contents_i18n`
-  ADD CONSTRAINT `gcv2contents_i18n_ibfk_1` FOREIGN KEY (`content_id`) REFERENCES `gcv2contents` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `FK_Article_Traduction` FOREIGN KEY (`content_id`) REFERENCES `gcv2contents` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
